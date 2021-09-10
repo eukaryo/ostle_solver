11fcdfe09ab3025f5a8d6795240514b86c3caec0
eukaryo <c3375c15@gmail.com>
2021-09-11 08:23:56 +0900

    Add files via upload

---
+++ estimate_positions_upperbound.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import re
+import datetime
+import pulp
+
+from math import factorial as fa
+
+a1 = (fa(25) // (fa(5) * fa(5) * fa(15))) * 15
+a2 = (fa(25) // (fa(5) * fa(4) * fa(16))) * 16 * 2
+a3 = (fa(25) // (fa(4) * fa(4) * fa(17))) * 17
+
+print(a1+a2+a3)
